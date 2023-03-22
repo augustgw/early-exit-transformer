3904ecf7f108557f13f119702a307960e85cc016
George Wright <augustgw@gmail.com>
2023-03-22 17:50:56 +0100

    Change dataset loader

--- data.py
+++ data.py
@@ -30,10 +30,7 @@ enc_voc_size = len(loader.source.vocab)
 dec_voc_size = len(loader.target.vocab)
 '''
 
-train_dataset1 = torchaudio.datasets.LIBRISPEECH("/falavi/corpora", url="train-clean-100", download=False)
-train_dataset2 = torchaudio.datasets.LIBRISPEECH("/falavi/corpora", url="train-clean-360", download=False)
-train_dataset3 = torchaudio.datasets.LIBRISPEECH("/falavi/corpora", url="train-other-500", download=False)
-train_dataset = torch.utils.data.ConcatDataset([train_dataset1,train_dataset2,train_dataset3]) 
+train_dataset = torchaudio.datasets.LIBRISPEECH("/corpora/LibriSpeech", url="train-clean-100", download=False)
 
 data_loader = torch.utils.data.DataLoader(train_dataset, pin_memory=False, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn, num_workers=num_workers)
 
