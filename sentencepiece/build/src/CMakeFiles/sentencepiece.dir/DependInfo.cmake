a01ecb687af8cd89731ef4bea0348994cdfdd763
George Wright <augustgw@gmail.com>
2023-06-07 16:17:54 +0200

    Add sentencepiece/build dir

---
+++ sentencepiece/build/src/CMakeFiles/sentencepiece.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/falavi/slu/sentencepiece/third_party/absl/flags/flag.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/absl/flags/flag.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/absl/flags/flag.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/arena.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/arena.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/arena.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/arenastring.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/arenastring.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/arenastring.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/bytestream.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/bytestream.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/bytestream.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/coded_stream.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/coded_stream.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/coded_stream.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/common.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/common.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/common.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/extension_set.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/extension_set.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/extension_set.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/generated_enum_util.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/generated_enum_util.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/generated_enum_util.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/generated_message_table_driven_lite.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/generated_message_table_driven_lite.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/generated_message_table_driven_lite.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/generated_message_util.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/generated_message_util.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/generated_message_util.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/implicit_weak_message.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/implicit_weak_message.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/implicit_weak_message.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/int128.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/int128.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/int128.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/io_win32.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/io_win32.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/io_win32.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/message_lite.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/message_lite.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/message_lite.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/parse_context.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/parse_context.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/parse_context.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/repeated_field.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/repeated_field.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/repeated_field.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/status.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/status.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/status.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/statusor.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/statusor.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/statusor.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/stringpiece.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/stringpiece.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/stringpiece.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/stringprintf.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/stringprintf.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/stringprintf.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/structurally_valid.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/structurally_valid.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/structurally_valid.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/strutil.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/strutil.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/strutil.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/time.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/time.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/time.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/wire_format_lite.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/wire_format_lite.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/wire_format_lite.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/zero_copy_stream.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/zero_copy_stream.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/zero_copy_stream.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/zero_copy_stream_impl.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/zero_copy_stream_impl.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/zero_copy_stream_impl.cc.o.d"
+  "/falavi/slu/sentencepiece/third_party/protobuf-lite/zero_copy_stream_impl_lite.cc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/zero_copy_stream_impl_lite.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/zero_copy_stream_impl_lite.cc.o.d"
+  "/falavi/slu/sentencepiece/src/bpe_model.cc" "src/CMakeFiles/sentencepiece.dir/bpe_model.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/bpe_model.cc.o.d"
+  "/falavi/slu/sentencepiece/src/builtin_pb/sentencepiece.pb.cc" "src/CMakeFiles/sentencepiece.dir/builtin_pb/sentencepiece.pb.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/builtin_pb/sentencepiece.pb.cc.o.d"
+  "/falavi/slu/sentencepiece/src/builtin_pb/sentencepiece_model.pb.cc" "src/CMakeFiles/sentencepiece.dir/builtin_pb/sentencepiece_model.pb.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/builtin_pb/sentencepiece_model.pb.cc.o.d"
+  "/falavi/slu/sentencepiece/src/char_model.cc" "src/CMakeFiles/sentencepiece.dir/char_model.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/char_model.cc.o.d"
+  "/falavi/slu/sentencepiece/src/error.cc" "src/CMakeFiles/sentencepiece.dir/error.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/error.cc.o.d"
+  "/falavi/slu/sentencepiece/src/filesystem.cc" "src/CMakeFiles/sentencepiece.dir/filesystem.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/filesystem.cc.o.d"
+  "/falavi/slu/sentencepiece/src/model_factory.cc" "src/CMakeFiles/sentencepiece.dir/model_factory.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/model_factory.cc.o.d"
+  "/falavi/slu/sentencepiece/src/model_interface.cc" "src/CMakeFiles/sentencepiece.dir/model_interface.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/model_interface.cc.o.d"
+  "/falavi/slu/sentencepiece/src/normalizer.cc" "src/CMakeFiles/sentencepiece.dir/normalizer.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/normalizer.cc.o.d"
+  "/falavi/slu/sentencepiece/src/sentencepiece_processor.cc" "src/CMakeFiles/sentencepiece.dir/sentencepiece_processor.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/sentencepiece_processor.cc.o.d"
+  "/falavi/slu/sentencepiece/src/unigram_model.cc" "src/CMakeFiles/sentencepiece.dir/unigram_model.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/unigram_model.cc.o.d"
+  "/falavi/slu/sentencepiece/src/util.cc" "src/CMakeFiles/sentencepiece.dir/util.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/util.cc.o.d"
+  "/falavi/slu/sentencepiece/src/word_model.cc" "src/CMakeFiles/sentencepiece.dir/word_model.cc.o" "gcc" "src/CMakeFiles/sentencepiece.dir/word_model.cc.o.d"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/falavi/slu/sentencepiece/build/src/libsentencepiece.so" "/falavi/slu/sentencepiece/build/src/libsentencepiece.so.0.0.0"
+  "/falavi/slu/sentencepiece/build/src/libsentencepiece.so.0" "/falavi/slu/sentencepiece/build/src/libsentencepiece.so.0.0.0"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sentencepiece/build/src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/io_win32.cc.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/sentencepiece.dir/__/third_party/protobuf-lite/io_win32.cc.o: \
+ /falavi/slu/sentencepiece/third_party/protobuf-lite/io_win32.cc \
+ /usr/include/stdc-predef.h
